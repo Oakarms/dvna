a8dd1260bee5dde834fa582f345c66cf2120691e
Oakarms <oakarms@gmail.com>
2020-07-22 09:32:41 -0500

    Add files via upload

--- LogoutAPI.java
+++ LogoutAPI.java
@@ -6,21 +6,17 @@ import javax.ws.rs.Path;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.Response;
 
-import com.ibm.security.appscan.altoromutual.util.ServletUtil;
-
 @Path("/logout")
 public class LogoutAPI extends AltoroAPI{
-
 	@GET
 	public Response doLogOut(@Context HttpServletRequest request){
-		
 		try{
 			request.getSession().removeAttribute(ServletUtil.SESSION_ATTR_USER);
 			String response="{\"LoggedOut\" : \"True\"}";
 			return Response.status(200).entity(response).build();}
 		catch(Exception e){
 			String response = "{\"Error \": \"Unknown error encountered\"}";
-			return Response.status(500).entity(response).build();
+			return Response.status(404).entity(response).build();
 		}
 	}
 }
